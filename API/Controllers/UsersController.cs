90c64a931fa5bdd418be3ea92582cb562f8a762d
Christine Wingfield <wingtek2020@gmail.com>
2024-10-17 10:32:33 -0400

    initial commit S2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Entities;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //api/users
+    public class UsersController(DataContext context) : Controller
+    {
+        // GET: Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var users = await context.Users.FindAsync(id);
+            if (users == null) return NotFound();
+
+            return users;
+        }
+
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string UserName { get; set; }
+    }
+}
